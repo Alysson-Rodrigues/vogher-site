ddcfbd878b8e2927065d3367083ef326bed54ff1
Alysson-Rodrigues <alyrodob@gmail.com>
2023-01-21 22:16:53 -0300

    :sparkles: feat/3d Rotating
    #

---
+++ src/ModelWrapper.tsx
@@ -0,0 +1,37 @@
+//Write a default export function that returns a function that returns a component
+//that renders the model and the text
+import React, { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { Model } from './Model'; /* highlight-line */
+
+export default function ModelWrapper() {
+
+   const [rotate, setRotate] = useState(0);
+
+   useEffect(() => {
+      const interval = setInterval(() => {
+        setRotate(prevRotate => prevRotate + 0.01);
+      }, 5);
+      return () => clearInterval(interval);
+    }, []);
+
+   return (
+      <Canvas
+         camera={{ position: [-23, 0, 30], fov: 3 }}
+         style={{
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            width: '100vw',
+            height: '100vh',
+         }}
+      >
+         <ambientLight intensity={'1'} />
+         <ambientLight intensity={0.1} />
+         <directionalLight intensity={0.4} />
+         <Suspense fallback={null}>
+         <Model position={[0.025, -1.5, 0]} rotation={[0,rotate,0]} />
+         </Suspense>
+         <OrbitControls />
+      </Canvas>
+   );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import ModelWrapper from '@/ModelWrapper'
 
 export default function Home() {
   return (
@@ -10,15 +11,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="bg-black h-screen w-full flex justify-center items-center p-12">
-        <div>
+        <div className="z-50">
           <img src="/voghe.svg" className="w-[15rem] py-9 -mt-10" />
           <div className='flex  text-4xl'>
+            
             <h2 className="text-white font-base-neue-bold-oblique py-1 ">Wait a little bit!</h2>
             <p className='animate-pulse'>🖐🏻</p>
           </div>
           <p className="text-white font-base-neue-light text-2xl">There will be a beautiful site here... but it's not ready yet</p>
         </div>
+        
       </main>
+      <div className="-mt-[100vh] z-10">
+      <ModelWrapper />
+      </div>
     </>
   )
 }
